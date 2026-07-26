3ba6c860f2b56f11a5cdf577de58ece41db258ce
TgYytg <cu6a.marta@gmail.com>
2026-07-26 17:39:19 +0300

    Update neuron.py

--- neuron.py
+++ neuron.py
@@ -7,14 +7,10 @@ class Trainer:
 
 class Neuron:
 
-    def __init__(self, weight, bias):
+    def __init__(self, weight = 0.00, bias = 0.00):
         self.weight = weight
         self.bias = bias
 
 
     def forward(self,x):
         return x * self.weight + self.bias
-
-    
-neuron = Neuron(0.01, -1.8)
-
