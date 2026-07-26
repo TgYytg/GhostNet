29ef45a5c55b26c8020452087f8ef629e3a162f7
TgYytg <cu6a.marta@gmail.com>
2026-07-26 17:50:06 +0300

    Update main.py
    Refactor training loop to include mean squared error calculation and print detailed output for each prediction.

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ data = [(0.150, 0),(0.160, 0),(0.170, 0),(0.180, 1),(0.190, 1)]
 
 learning_rate = 0.00001
 neuron = Neuron(0, 0)
+total_loss = 0
+def mse(target, prediction):
+    return (target - prediction) **2
 #процесс обучения
-for epoch in range(100):
+for x, target in data:
     total_error = 0
 
     for height, target in data:
 
-        prediction = neuron.forward(height)
+        prediction = neuron.forward(x)
+        loss = mse(target, prediction)
         error = target - prediction
+        
         total_error += abs(error)
+        total_loss += loss
         neuron.weight += error * height * learning_rate
 
     average_error = total_error / len(data)
 
-    print(epoch, average_error)
+     print(
+        f"x={x} "
+        f"prediction={prediction:.3f} "
+        f"target={target} "
+        f"loss={loss:.3f}"
+    )
 
-print(neuron.weight)  
+print("Average loss:", total_loss / len(data))
